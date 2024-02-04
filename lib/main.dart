87ef5656bc87b6abda2778e1b12bbdc93046ed81
Aryan Chachra <aryanchachra1406@gmail.com>
2024-02-04 22:39:56 +0530

    Day 11 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:course_30daysofflutter/utils/routes.dart';
 import 'package:course_30daysofflutter/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
-
+//In Day 11, learnt about widget Tree, render tree and element tree also learnt about how the positioning works with parent widget and learnt the concept of constraints and context.
 void main() {
   runApp(MyApp());
 }
