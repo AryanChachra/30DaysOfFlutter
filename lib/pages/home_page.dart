0340f20f99031d304cd446233b8ff98fc3386438
Aryan Chachra <aryanchachra1406@gmail.com>
2024-02-05 02:00:31 +0530

    Day 12 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,31 @@
+import 'package:course_30daysofflutter/models/catalog.dart';
 import 'package:flutter/material.dart';
-
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final int days = 30;
+  final String name = "Aryan";
 
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "Aryan";
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome to $days Days of Flutter by $name'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          // itemCount: CatalogModel.items.length,
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            // return ItemWidget(item: CatalogModel.items[index]);
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
