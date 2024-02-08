3908a79f3d139653bbff0b0a5e58db3776983b97
Aryan Chachra <aryanchachra1406@gmail.com>
2024-02-09 00:45:25 +0530

    Day 13 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,35 @@
 import 'package:course_30daysofflutter/models/catalog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Aryan";
+class HomePage extends StatefulWidget {
 
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Aryan";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
